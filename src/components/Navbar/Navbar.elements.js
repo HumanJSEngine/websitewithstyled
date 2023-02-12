40a46287c05b9ee794f7c5b3bea773d646041d5f
bmj <bmj44571627@gmail.com>
2023-02-13 07:46:17 +0900

    기본세팅

---
+++ src/components/Navbar/Navbar.elements.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+background:#101522;
+height:80px;
+display:flex;
+justify-content: center;
+align-items: center;
+font-size:1.2rem;
+position:sticky;
+top:0;
+position: sticky;
+z-index:100;
+
+&:hover{
+  color:red;
+}
+` 
